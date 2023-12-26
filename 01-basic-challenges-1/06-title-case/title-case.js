fe2e98fd50c83842a3fe4fbc279e8718a3fd35b7
Michael Neely <michael@michaelneely.net>
2023-12-26 12:36:09 -0500

    Completed the Title case challenge

--- 01-basic-challenges-1/06-title-case/title-case.js
+++ 01-basic-challenges-1/06-title-case/title-case.js
@@ -1,3 +1,10 @@
-function titleCase() {}
+function titleCase(str) {
+    const splitStr = str.toLowerCase().split(' '); 
+    
+    for (let i = 0; i < splitStr.length; i++) {
+        splitStr[i] = splitStr[i][0].toUpperCase() + splitStr[i].slice(1);
+    }
+    return splitStr.join(' ');;
+}
 
 module.exports = titleCase;
