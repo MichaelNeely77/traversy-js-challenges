dc31f5cc51cfa30f10649f0e5e50c6f97721f328
Michael Neely <michael@michaelneely.net>
2024-07-17 09:56:50 -0400

    Completed the reverse string section

--- 01-basic-challenges-1/07-reverse-string/reverse-string.js
+++ 01-basic-challenges-1/07-reverse-string/reverse-string.js
@@ -1,3 +1,19 @@
-function reverseString() {}
+// Solution 1
+
+// function reverseString(str) {
+//   return str.split('').reverse().join('');
+// }
+
+
+// Soluton Two
+// const reverseString = (str) => str.split('').reverse().join('');
+
+function reverseString(str) {
+  let reversed = '';
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed += str[i];
+  }
+  return reversed;
+}
 
 module.exports = reverseString;
