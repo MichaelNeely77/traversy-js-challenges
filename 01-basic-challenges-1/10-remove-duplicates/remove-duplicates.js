0886de3cc6bd4f18c00f61e93ecfceed4bca2a23
Michael Neely <michael@michaelneely.net>
2024-07-18 11:38:08 -0400

    completed the count vowels and remove duplicatges lesson

--- 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
+++ 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
@@ -1,3 +1,20 @@
-function removeDuplicates() {}
+// Solution 1
+
+// function removeDuplicates(arr) {
+//   const uniqueArr = [];
+
+//   for (let i = 0; i < arr.length; i++) {
+//     if (!uniqueArr.includes(arr[i])) {
+//       uniqueArr.push(arr[i]);
+//     }
+//   }
+//   return uniqueArr;
+// }
+
+// Solution 2
+
+function removeDuplicates(arr) {
+  return Array.from(new Set(arr));
+}
 
 module.exports = removeDuplicates;
