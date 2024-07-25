c6b350a788a1417829ff89e11ad2e9f2f053e517
Michael Neely <michael@michaelneely.net>
2024-07-25 11:57:00 -0400

    Updating three lessons completed.

--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,3 +1,28 @@
-function arrayIntersection() {}
+// Solution 1
+
+// function arrayIntersection(arr1, arr2) {
+//   const intersectionArray = [];
+
+//   for (let i = 0; i < arr1.length; i++) {
+//     if (arr2.includes(arr1[i]) && !intersectionArray.includes(arr1[i])) {
+//       intersectionArray.push(arr1[i]);
+//     }
+//   }
+//   return intersectionArray;
+// }
+
+// Solution 2
+
+function arrayIntersection(arr1, arr2) {
+  const set1 = new Set(arr1);
+  const intersectionArr = [];
+
+  for (let num of arr2) {
+    if (set1.has(num)) {
+      intersectionArr.push(num);
+    }
+  }
+  return intersectionArr;
+}
 
 module.exports = arrayIntersection;
