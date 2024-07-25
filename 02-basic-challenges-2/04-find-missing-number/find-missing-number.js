eb8e490a5c65b24ceb6a22e6f9e09355d15a25e6
Michael Neely <michael@michaelneely.net>
2024-07-25 12:09:25 -0400

    Added the find-missing-number lesson completion

--- 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
+++ 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
@@ -1,3 +1,22 @@
-function findMissingNumber() {}
+function findMissingNumber(arr) {
+  if(arr.length === 0) return 1;
+
+  const n = arr.length +1;
+  const expectedSum = n * (n + 1) / 2;
+  
+  // Solution 1
+//   let actualSum = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     actualSum += arr[i];
+//   }
+//   return expectedSum - actualSum;
+// }
+
+//Solution 2
+
+const actualSum = arr.reduce((sum, number) => sum + number, 0 );
+
+return expectedSum - actualSum;
+}
 
 module.exports = findMissingNumber;
